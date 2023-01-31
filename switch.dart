4cfdb9c6a0319fd03c5060fa749a9c150e612167
banjo254 <benjaminmariga@gmail.com>
2023-01-31 07:01:59 +0300

    switch statement

---
+++ switch.dart
@@ -0,0 +1,20 @@
+void main() {
+  String grade = 'h';
+  switch (grade) {
+    case 'A':
+      print("exellent");
+      break;
+    case 'B':
+      print("Very Good");
+      break;
+    case 'C':
+      print("Average");
+      break;
+    case 'D':
+      print("Fail");
+      break;
+    default:
+      print("invalid grade entry");
+      break;
+  }
+}
