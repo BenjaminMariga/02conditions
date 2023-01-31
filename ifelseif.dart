6d33d2197a01785205dbbc5bdd9e5b5b8c49d016
banjo254 <benjaminmariga@gmail.com>
2023-01-31 06:54:23 +0300

    if else if statement

---
+++ ifelseif.dart
@@ -0,0 +1,15 @@
+void main() {
+  //simple if else if to display colours of the kenyan flag
+  String colour = "red";
+  if (colour == "red") {
+    print("blood");
+  } else if (colour == "white") {
+    print("peace");
+  } else if (colour == "black") {
+    print("people");
+  } else if (colour == "green") {
+    print("vegetation");
+  } else {
+    print("invalid entry");
+  }
+}
